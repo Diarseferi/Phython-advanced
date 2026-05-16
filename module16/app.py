90ea9a38b7d7632d3827c4d8a6a75f2ec0bbbfed
Diarseferi <diarseferi23@gmail.com>
2026-05-16 11:14:31 +0200

    changes

--- module16/app.py
+++ module16/app.py
@@ -12,6 +12,18 @@ def main():
     streamlit.write("You wrote:",user_input)
     age = streamlit.number_input("Enter your age", min_value=0,max_value=100)
     streamlit.write(f"Your age is:{age}")
+    message = streamlit.text_area("Enter a message")
+    streamlit.write(f"Your message:{message}")
+    choice = streamlit.radio("Pick one",["Choice 1","Choice 2","Choice 3"])
+    streamlit.write(f"You chose:{choice}")
+    if streamlit.button("Success"):
+        streamlit.success("operation  was successful")
+
+    try:
+        1/0
+    except Exception as e:
+        streamlit.exception(e)
+
 
 if __name__ == "__main__":
     main()
