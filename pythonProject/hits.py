a767bbad055df6c455e9af7e6aea4ea73c250e2f
Diarseferi <diarseferi23@gmail.com>
2026-06-27 12:40:39 +0200

    changes

---
+++ pythonProject/hits.py
@@ -0,0 +1,6 @@
+def getFullName(firstName,lastName):
+    fullName=firstName+lastName
+    return fullName
+
+
+print(getFullName("Diar","Seferi"))
---
+++ pythonProject/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"Hello,World"}
+
+@app.get("/kurset")
+def return_kurset():
+    return{"kurset":["Python Adv","Ai","php"]}
+
+@app.get("/kurset/{kursi_id}")
+def return_kurset(item_id:int):
+    return{"kurset":["Python Adv","Ai","php"]}
+
+@app.put("/register")
+def create_user(name:str,password:str):
+    return{"user_id":name,"password":password}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id:int):
+    return{"message":"Item is deleted"}
+
+@app.put("/user/password/{user_id}")
+def change_Password(user_id:int,password:str):
+    return{"message":"passwordi u ndryshua me sukses"}
